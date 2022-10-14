29a9b963b6ab3d0e788d43da21126c1161197b0e
Valeriy Fyodorov <valeriy.fyodorov@gmail.com>
2022-10-15 00:25:34 +0300

    some tweaking

--- rc/helpers/ocr.py
+++ rc/helpers/ocr.py
@@ -6,6 +6,8 @@ import time
 import os
 import numpy as np
 
+EASY_MIN_CONFIDENCE_LEVEL = 0.55
+
 
 def processText(txt):
     false_flags = [",", '"', " ", "!", "\n", "'"]
@@ -23,60 +25,22 @@ def extract_tesser(image):
 
 def extract_easy(reader, image):
     box = reader.readtext(image)
+    conf = 0
     res = ""
     if len(box) > 0:
         res = processText(box[0][1])
-    return res
-
-
-def auto_contrast(image, clip_hist_percent=1):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # Calculate grayscale histogram
-    hist = cv2.calcHist([gray], [0], None, [256], [0, 256])
-    hist_size = len(hist)
-    # Calculate cumulative distribution from the histogram
-    accumulator = []
-    accumulator.append(float(hist[0]))
-    for index in range(1, hist_size):
-        accumulator.append(accumulator[index - 1] + float(hist[index]))
-    # Locate points to clip
-    maximum = accumulator[-1]
-    clip_hist_percent *= (maximum/100.0)
-    clip_hist_percent /= 2.0
-    # Locate left cut
-    minimum_gray = 0
-    while accumulator[minimum_gray] < clip_hist_percent:
-        minimum_gray += 1
-    # Locate right cut
-    maximum_gray = hist_size - 1
-    while accumulator[maximum_gray] >= (maximum - clip_hist_percent):
-        maximum_gray -= 1
-    # Calculate alpha and beta values
-    alpha = 255 / (maximum_gray - minimum_gray)
-    beta = -minimum_gray * alpha
-    auto_result = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
-    return auto_result
+        if len(box[0]) > 2:
+            conf = box[0][2]
+    return res, conf
 
 
 def processImage(image, resize=False):
-    # # first try to gray
-    # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # (thresh, image) = cv2.threshold(image, 127, 255, cv2.THRESH_OTSU)
-    # # try resize
-    # auto contrast
-    # image = auto_contrast(image)
     if image.shape[1] < 80 or image.shape[1] > 300 or resize:
         new_width = 256
         new_height = int(new_width / image.shape[1] * image.shape[0])
         image = cv2.resize(image,
                            (new_width, new_height),
                            interpolation=cv2.INTER_CUBIC)
-    # # try dilate / erode
-    # kernel = np.ones((3, 3), np.uint8)
-    # image = cv2.erode(image, kernel, iterations=1)
-    # image = cv2.dilate(image, kernel, iterations=1)
-    # cv2.imshow('Black white image', image)
-    # cv2.waitKey(2000)
     return image
 
 
@@ -107,7 +71,28 @@ def run_tests(file_names):
 
 
 def extract_result(image, reader):
-    return extract_tesser(processImage(image)), extract_easy(reader, processImage(image, resize=True))
+    width = image.shape[1]
+    if image.shape[0] < 10 or width < 20:
+        return "", 0
+    tesser_confid = 0.4
+    if width < 100:
+        tesser_confid = 0.3
+    # print(width)
+    if width > 150:
+        result, confid = extract_easy(reader, processImage(image, resize=True))
+    else:
+        result, confid = "", 0.2
+    # print(result, confid)
+    # try tesser where easy confidence is low
+    if confid < EASY_MIN_CONFIDENCE_LEVEL or len(result) < 8:
+        text_tesser = extract_tesser(processImage(image))
+        if len(text_tesser) >= 8:
+            result = text_tesser
+            confid = tesser_confid
+            # print(result, confid, "----tesser---")
+    if len(result) < 8:
+        result = ""
+    return result, confid
 
 
 if __name__ == '__main__':
--- rc/run.py
+++ rc/run.py
@@ -2,8 +2,17 @@ import cv2
 import numpy as np
 import time
 import easyocr
+import re
+from itertools import groupby
 from helpers.infer import pre_process, post_process
 from helpers.ocr import extract_result
+
+CAMERA_BUFFER_SIZE = 3
+
+
+def all_equal(iterable):
+    g = groupby(iterable)
+    return next(g, True) and not next(g, False)
 
 
 def inferFrame(frame, net):
@@ -11,23 +20,66 @@ def inferFrame(frame, net):
     return post_process(frame.copy(), detections)
 
 
-def processStream():
-    net = cv2.dnn.readNet('../models/best_simp.onnx')
-    reader = easyocr.Reader(['en'])
-    cap = cv2.VideoCapture('01.ts')
+def chooseBest(results, confids):
+    results = [re.sub("[^0-9]", "", ss) for ss in results]
+    if len(results) == 1 and len(results[0]) == 8:
+        return results[0]
+    def_result = "XXXXXXXX"
+    if len(results) == 0:
+        return def_result
+    max_conf_value = max(confids)
+    max_conf_index = confids.index(max_conf_value)
+    highest_conf_result = results[max_conf_index]
+    if len(highest_conf_result) != 8:
+        return def_result
+    else:
+        return highest_conf_result
+
+
+def processDetectionInImage(img, reader):
+    return extract_result(img, reader)
+    # print(extract_result(img, reader))
+
+
+def processFrame(frame, net, reader):
+    imgs = inferFrame(frame, net)
+    results = []
+    confids = []
+    for img in imgs:
+        found, conf = processDetectionInImage(img, reader)
+        if found != "":
+            results.append(found)
+            confids.append(conf)
+    plate = chooseBest(results, confids)
+    # print(plate)
+    # if plate != "XXXXXXXX":
+    #     print("-------detected----------")
+    return plate
+
+
+def processStream(net, reader):
+    cap = cv2.VideoCapture('03.mp4')
+    cap.set(cv2.CAP_PROP_BUFFERSIZE, CAMERA_BUFFER_SIZE)
     if (cap.isOpened() == False):
         print("Error opening video stream or file")
     # Read until video is completed
+    latest_detection = "XXXXXXXX"
+    candidates = ["a", "b", "c", "d", "e", "f"]
     while (cap.isOpened()):
         # Capture frame-by-frame
         ret, frame = cap.read()
         if ret == True:
-            start = time.time()
-            inferFrame(frame, net)
-            end = time.time()
-            print("[INFO] Frame detection function took {:.6f} s".format(
-                end - start))
-            if cv2.waitKey(25) & 0xFF == ord('q'):
+            candidate = processFrame(frame, net, reader)
+            if candidate != "XXXXXXXX":
+                candidates.insert(0, candidate)
+                candidates.pop()
+                # print(candidates)
+                if all_equal(candidates) and candidates[0] != "XXXXXXXX" and candidates[0] != latest_detection:
+                    latest_detection = candidates[0]
+                    if len(latest_detection) == 8:
+                        print(
+                            f"Found good detection {latest_detection} - congrats")
+            if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
         # Break the loop
         else:
@@ -38,20 +90,14 @@ def processStream():
     cv2.destroyAllWindows()
 
 
-def processFrame(frame, net, reader):
-    inferFrame(frame, net)
-    print(extract_result(img, reader))
-
-
 if __name__ == '__main__':
     frame = cv2.imread('test.jpg')
     net = cv2.dnn.readNet('../models/best_simp.onnx')
     reader = easyocr.Reader(['en'])
-    imgs = inferFrame(frame, net)
-    print("Start frame ocr")
     start = time.time()
-    for img in imgs:
-        processFrame(frame, net, reader)
+    # processFrame(frame, net, reader)
+    processStream(net, reader)
+    # print(chooseBest([]))
     end = time.time()
     print("[INFO] Frame detection function took {:.6f} s".format(
         end - start))
