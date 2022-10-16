3e1c859b556566dd184418cbecea1cea2d8fe258
Valeriy Fyodorov <valeriy.fyodorov@gmail.com>
2022-10-16 14:15:32 +0300

    Some ocr sharpen tests

--- ocr/ocr_test.py
+++ ocr/ocr_test.py
@@ -7,7 +7,10 @@
 # pip uninstall opencv-contrib-python-headless
 # pip3 install opencv-contrib-python==4.5.5.62
 from paddleocr import PaddleOCR
-
+from scipy.signal import fftconvolve
+from skimage import color, data, restoration
+from skimage.util import img_as_float
+from scipy.signal import convolve2d
 import pytesseract
 import cv2
 import easyocr
@@ -41,6 +44,7 @@ def extract_paddle(ocr, image, file):
     result = ocr.ocr(image, cls=True)
     res = ""
     for box in result:
+        # print(box)
         if len(box) > 0:
             if len(box[0]) > 1:
                 txt, conf = box[0][1]
@@ -61,6 +65,49 @@ def extract_easy(reader, image, file):
     if len(box) > 0:
         res = processText(box[0][1])
     print(res, res == file[:8], file)
+
+
+# +-128 for brightness, +-64 for contrast
+def apply_brightness_contrast(img, brightness=-80, contrast=90):
+    input_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    if cv2.mean(input_img)[0] < 128:
+        return img
+    if brightness != 0:
+        if brightness > 0:
+            shadow = brightness
+            highlight = 255
+        else:
+            shadow = 0
+            highlight = 255 + brightness
+        alpha_b = (highlight - shadow)/255
+        gamma_b = shadow
+        buf = cv2.addWeighted(input_img, alpha_b, input_img, 0, gamma_b)
+    else:
+        buf = input_img.copy()
+    if contrast != 0:
+        f = 131*(contrast + 127)/(127*(131-contrast))
+        alpha_c = f
+        gamma_c = 127*(1-f)
+        buf = cv2.addWeighted(buf, alpha_c, buf, 0, gamma_c)
+    return apply_filter(buf)
+
+
+def apply_filter(image):
+    kernel2d = np.array([[0, 0,  0],
+                         [-1,  4, -1],
+                         [0, 0,  0]])
+    result = cv2.filter2D(src=image, ddepth=-1, kernel=kernel2d)
+    return result
+
+
+def testDisplaySharpenedImage(file_name):
+    contrasted = apply_brightness_contrast(cv2.imread(file_name))
+    # filtered = apply_filter(contrasted)
+    filtered = apply_filter(contrasted)
+    cv2.imshow('Contrasted', contrasted)
+    cv2.imshow('Filtered', filtered)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
 
 def auto_contrast(image, clip_hist_percent=1):
@@ -99,6 +146,7 @@ def processImage(image, resize=False):
     # # try resize
     # auto contrast
     # image = auto_contrast(image)
+    image = apply_brightness_contrast(image)
     if image.shape[1] < 80 or image.shape[1] > 300 or resize:
         new_width = 256
         new_height = int(new_width / image.shape[1] * image.shape[0])
@@ -147,6 +195,30 @@ def run_tests(file_names):
     print("[INFO] Easy took {:.6f} seconds".format(time.time() - start))
 
 
+def displayApplyBrigtnessContrast(file_name, brightness, contrast):
+    img = cv2.imread(file_name)
+    cv2.imshow(file_name, apply_brightness_contrast(img, brightness, contrast))
+    cv2.waitKey(2000)
+    cv2.destroyAllWindows()
+
+
+def testSaveImageApplyBrigtnessContrast(file_name):
+    img = cv2.imread(file_name)
+    for b in range(-80, -60, 4):
+        for c in range(80, 120, 4):
+            cv2.imwrite(f"frames/b{b:03d}c{c:03d}.jpg",
+                        apply_brightness_contrast(img, b, c))
+    cv2.destroyAllWindows()
+
+
+def run_save_correction_tests(file_names, brightness, contrast):
+    for file in file_names:
+        testSaveImageApplyBrigtnessContrast(file, brightness, contrast)
+
+
 if __name__ == '__main__':
     file_names = jpgsIntoList('.')
+    # run_save_correction_tests(file_names, -64, 96)
+    # testSaveImageApplyBrigtnessContrast("95534483_l.jpg")
+    # testDisplaySharpenedImage("95534483_l.jpg")
     run_tests(file_names)
