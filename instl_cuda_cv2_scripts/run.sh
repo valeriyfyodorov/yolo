89fb62a8b89af21e53baadf841102049c3928d37
Valeriy Fyodorov <valeriy.fyodorov@gmail.com>
2022-11-16 19:16:27 +0200

    Starting bash file

--- instl_cuda_cv2_scripts/run.sh
+++ instl_cuda_cv2_scripts/run.sh
@@ -2,19 +2,19 @@
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 if command -v pyenv 1>/dev/null 2>&1; then
- eval "$(pyenv init -)"
+    eval "$(pyenv init -)"
 fi
 eval "$(pyenv virtualenv-init -)"
 export PATH=/usr/local/cuda-11.8/bin${PATH:+:${PATH}}:~/bin
-export LD_LIBRARY_PATH=/usr/local/cuda-11.8/lib64${LD_LIBRARY_PATH:+:${LD_LIBRA>
+export LD_LIBRARY_PATH=/usr/local/cuda-11.8/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
 
 cd ~/Desktop/rcars/rc
 pyenv activate rcars
 if [ -z "$1" ]
-  then
+then
     echo "No argument supplied"
-  else
+else
     echo "Starting python script"
     python run_$1.py
-  fi
+fi
